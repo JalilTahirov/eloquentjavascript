820ebb223cc03d9a2512354db8c05e210f5d52d3
Jalil Tahirov <jalil.tahirov@outlook.com>
2019-10-11 23:29:14 +0400

    till mutability

---
+++ DataStructuresObjectsandArrays/script.js
@@ -0,0 +1,53 @@
+const myUpperCase = (doh) => {
+    console.log(typeof doh.toUpperCase);
+    console.log(doh.toUpperCase());
+}
+myUpperCase("Джали");
+
+//.....
+
+const myLowerCase = (doh) => {
+    console.log(typeof doh.toLowerCase);
+    console.log(doh.toLowerCase());
+}
+myLowerCase("DDDsssaaDD")
+
+//  В следующем примере демонстрируются некоторые методы, имеющиеся у массивов:
+
+const arrayFunctions = () => {
+    var mack = [];
+    mack.push("trest,");
+    mack.push("kotoriy","lopnul");
+    console.log(mack);
+    console.log(mack.join(" "));
+    console.log(mack.pop());
+    console.log(mack);
+}
+
+arrayFunctions();
+
+
+// Один из способов создать объект – использовать фигурные скобки:
+var day1 = {
+    squirrel: false,
+    events: ["rabota","tronul derevo","picca","probejka"],
+};
+
+console.log(day1.squirrel);
+console.log(day1.wolf);
+day1.wolf = false;
+console.log(day1.wolf);
+
+// Оператор delete отрезает щупальце. Это унарный оператор, 
+//применяемый к выражению доступа к свойству.
+var anObject = {left: 1,right: 2};
+console.log(anObject.left);
+
+delete anObject.left;
+console.log(anObject.left);
+console.log("left" in anObject);
+console.log("right" in anObject);
+
+
+
+
